df5e2a8861df4e5bd378258923fdb051a20274ec
Daniel Lee <daniel.lee011106@gmail.com>
2023-06-17 21:34:12 +0700

    fully functional update

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    
+    let nav_list = document.querySelectorAll("li")
+    let a_list  = document.querySelectorAll(".nav--link")
+
+    for (let i = 0; i < nav_list.length; i++) {
+        nav_list[i].addEventListener("click", function(){
+            for (let n = 0; n <nav_list.length; n++){
+                nav_list[n].classList.remove("clicked")
+                a_list[n].classList.remove("clicked")          
+              }
+            this.classList.add("clicked")
+            a_list[i].classList.add("clicked")
+        })
+
+        
+      }
 })
