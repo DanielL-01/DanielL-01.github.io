6284780387b99ad586e85a7e55afb4366ec52911
Daniel Lee <daniel.lee011106@gmail.com>
2023-06-17 22:37:52 +0700

    finishing

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 document.addEventListener("DOMContentLoaded", ()=>{
-    let nav_list = document.querySelectorAll("li")
-    let a_list  = document.querySelectorAll(".nav--link")
+  let nav_list = document.querySelectorAll("li")
+  let a_list  = document.querySelectorAll(".nav--link")
 
-    for (let i = 0; i < nav_list.length; i++) {
-        nav_list[i].addEventListener("click", function(){
-            for (let n = 0; n <nav_list.length; n++){
-                nav_list[n].classList.remove("clicked")
-                a_list[n].classList.remove("clicked")          
-              }
-            this.classList.add("clicked")
-            a_list[i].classList.add("clicked")
-        })
+  for (let i = 0; i < nav_list.length; i++) {
+      nav_list[i].addEventListener("click", function(){
+          for (let n = 0; n <nav_list.length; n++){
+              nav_list[n].classList.remove("clicked")
+              a_list[n].classList.remove("clicked")          
+            }
+          this.classList.add("clicked")
+          a_list[i].classList.add("clicked")
+      })
 
-        
-      }
+      
+    }
+
+    document.querySelector('.burger'). addEventListener('click', function(){
+      document.querySelector('ul').classList.toggle('clicked');
+  })
 })
